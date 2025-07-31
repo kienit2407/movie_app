93c24722b4c7bc930ab1fd09ebee331a7691a28e
kinit <kinit@Mac-buc-cua-kin.local>
2025-07-31 07:15:23 +0700

    completed Task 1

---
+++ lib/core/config/themes/app_color.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const bgApp = Color(0xff191A24);
+  static const primaryColor = LinearGradient(colors: [
+      Color(0xffec008c),
+      Color(0xffF4347A),
+      Color(0xfffc6767),
+    ],
+    begin: Alignment.centerLeft,
+    end: Alignment.centerRight
+  );
+  static const buttonColor = Color(0xff36383F);
+  static const secondColor = Color(0xffE21120);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:movie_app/core/config/themes/app_theme.dart';
+import 'package:movie_app/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MovieApp());
 }
- class MovieApp extends StatelessWidget {
+class MovieApp extends StatelessWidget {
   const MovieApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SystemChrome.setSystemUIOverlayStyle( 
+      const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      )
+    );
+    return MaterialApp(
+      theme: AppTheme.appTheme,
+      debugShowCheckedModeBanner: false,
+      home: const SplashPage(),
+    );
   }
 }
-
-//step by step to build movie app
-/*
-//Write anything at here
- */
